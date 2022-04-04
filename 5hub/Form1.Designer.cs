74b7062f162ad49f81467acebf846f3cef25f9d2
5gi <5giwastooshort@gmail.com>
2022-04-04 13:21:00 -0400

    Test Commit 2
    2

--- 5hub/Form1.Designer.cs
+++ 5hub/Form1.Designer.cs
@@ -28,13 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.devops = new System.Windows.Forms.GroupBox();
+            this.clickc = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(802, 451);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // devops
+            // 
+            this.devops.Location = new System.Drawing.Point(588, 101);
+            this.devops.Name = "devops";
+            this.devops.Size = new System.Drawing.Size(200, 210);
+            this.devops.TabIndex = 1;
+            this.devops.TabStop = false;
+            this.devops.Text = "DevOptions";
+            this.devops.Visible = false;
+            // 
+            // clickc
+            // 
+            this.clickc.AutoSize = true;
+            this.clickc.Location = new System.Drawing.Point(736, 428);
+            this.clickc.Name = "clickc";
+            this.clickc.Size = new System.Drawing.Size(52, 13);
+            this.clickc.TabIndex = 2;
+            this.clickc.Text = "Test APP";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.clickc);
+            this.Controls.Add(this.devops);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "5hub";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox devops;
+        private System.Windows.Forms.Label clickc;
     }
 }
 
--- 5hub/Form1.cs
+++ 5hub/Form1.cs
@@ -11,10 +11,36 @@ using System.Windows.Forms;
 namespace _5hub
 {
     public partial class Form1 : Form
-    {
+    { 
         public Form1()
         {
+
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            int clicks = 0;
+            int until = 20 - clicks;
+            string yet = until.ToString();
+            if (clicks == 20)
+            {
+                devops.Visible = true;
+                clickc.Text = "Test APP";
+            } else
+            {
+                clicks++;
+            }
+
+            if (clicks <= 19) {
+                if (clicks >= 5)
+                {
+                    clickc.Text = yet;
+                }
+            } else
+            {
+                clickc.Text = "TestAPP";
+            }
         }
     }
 }
